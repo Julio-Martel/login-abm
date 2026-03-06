fb043fde6d2cfe27dd07ccce4ba6c30de8d4db80
Julio-Martel <julioramdom123@gmail.com>
2026-03-06 01:28:33 -0300

    se ha creado la verificacion de usuario

--- server.js
+++ server.js
@@ -1,4 +1,33 @@
 const express = require('express');
 const aplicacion = express();
-const login = require('./login/login');
+const usuariosRegistrados = require('./login/login');
 
+aplicacion.use(express.json());
+
+aplicacion.get('/login',(req,res) => {
+    res.json(usuariosRegistrados);
+})
+
+aplicacion.post('/login',(req,res) => {
+    const nuevoUsuario = {
+        usuario: req.body.usuario,
+        password: req.body.password
+    }
+
+    const logeado = usuariosRegistrados.find(us => (us.usuario === nuevoUsuario.usuario && (us.password === nuevoUsuario.password)));
+
+    if(logeado){
+        res.status(200).json({
+            exito: 'El usuario ha sido logeado correctamente'
+        })
+    } else {
+        res.status(400).json({
+            error: 'El usuario o password es incorrecto'
+        })
+    }
+
+})
+
+aplicacion.listen(3000,()=>{
+    console.log('Servidor en funcionamiento');
+})
